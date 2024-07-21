82f7d04129ca42b049a286a7ba39afb83f509226
NamanAggarwal21 <121608569+NamanAggarwal21@users.noreply.github.com>
2024-07-21 16:57:11 +0530

    Update orderController.js

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async(req,res)=>{
 
-    const frontend_url = 'http://localhost:5174'
+    const frontend_url = 'https://artvendor.onrender.com/'
 
     try {
         const newOrder = new orderModel({
