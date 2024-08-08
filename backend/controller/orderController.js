3dac4fdc77e7ef6fe7802b88641d5de62e51b29a
NamanAggarwal21 <121608569+NamanAggarwal21@users.noreply.github.com>
2024-08-09 00:15:39 +0530

    Update orderController.js
    added payment_method_types:["card"] in placeorder

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -53,6 +53,7 @@ const placeOrder = async(req,res)=>{
         const session = await stripe.checkout.sessions.create({
             line_items:line_items,
             mode:'payment',
+            payment_method_types:["card"],
             // defining payment fail or success ?
             success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
             cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
