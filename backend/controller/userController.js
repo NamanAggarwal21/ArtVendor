d255cc238d9f0c3ebe534ad8837bba996a955b94
NamanAggarwal21 <121608569+NamanAggarwal21@users.noreply.github.com>
2024-08-09 18:08:53 +0530

    Update userController.js
    IsMatch value is changed

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -19,7 +19,7 @@ const loginUser = async(req,res)=>{
             })
         }
         const isMatch = await bcrypt.compare(password , user.password);
-        if(isMatch){
+        if(!isMatch){
             return res.json({
                 success:false,
                 message:"Invalid Credentials"
